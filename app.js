ee61cfcf8c2a63fc3c041309069abe8198436774
mohamedkhaled22669 <mohamedkhaled22669@gmail.com>
2019-10-03 17:30:57 +0200

    ss

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const db = require("./config/database");
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 
 
@@ -29,7 +29,7 @@ app.get('/', (req,res)=> {
 const events = require('./routes/event-routes');
 app.use('/events' , events)
 
-app.listen(port, ()=> {
+app.listen(PORT, ()=> {
 
-    console.log(`this is working.....port ${port}`);
+    console.log(`this is working.....port ${PORT}`);
 });

8cbdb2bfdfe52af594c36cc0bacd10c6cde45543
mohamedkhaled22669 <mohamedkhaled22669@gmail.com>
2019-10-03 17:33:39 +0200

    s

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/eventsDB' ||'mongodb://user:01140305151@ds229118.mlab.com:29118/heroku_pcbrk11v',
+mongoose.connect( 'mongodb://user:01140305151@ds229118.mlab.com:29118/heroku_pcbrk11v' ||'mongodb://localhost:27017/eventsDB',
 {useNewUrlParser: true , useUnifiedTopology: true } , 
 (err) => {
     

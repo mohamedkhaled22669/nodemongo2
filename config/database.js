708dd9f17a8d69902a2aaef02725ad650c061400
mohamedkhaled22669 <51810133+mohamedkhaled22669@users.noreply.github.com>
2019-10-03 18:09:18 +0200

    d
    d

--- config/database.js
+++ config/database.js
@@ -1,13 +1,1 @@
-const mongoose = require("mongoose");
 
-mongoose.connect( 'mongodb://user:01140305151@ds229118.mlab.com:29118/heroku_pcbrk11v' ||'mongodb://localhost:27017/eventsDB',
-{useNewUrlParser: true , useUnifiedTopology: true } , 
-(err) => {
-    
-    if (err) {
-        
-        console.log(err)
-    }else{
-        console.log('connected to db succesfuly...')
-    }
-});
